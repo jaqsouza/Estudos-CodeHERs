71686811ddb0b7e98f6e97de08d0102bf77516c0
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-08-30 11:30:41 -0300

    add aprendendo_int_e_chair.c

---
+++ apendendo_int_e_char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 42;
+    char c = 'A';
+
+    printf("número: %d\n", x);
+    printf("letra: %c\n", c);
+    printf("Cod. ASCII: %d!\n", c);
+    printf("Hex 2dig: %02x\n", x);
+    return 0;
+}
