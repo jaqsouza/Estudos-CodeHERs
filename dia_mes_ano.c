a7cb8c8d4647824186fc6255b630e2ddeea06ba6
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-08-30 11:27:34 -0300

    add dia_mes_ano

---
+++ dia_mes_ano.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int ano = 2026;
+    int mês = 8;
+    int dia = 19
+
+    printf("Ano em que participei do CodeHERs: %d\n", ano);
+    printf("Mês que comecei o programa: %d\n", mês);
+    printf("Primeiro dia de aula de C: %d\n", dia);
+    return 0;
+}
