7f86869f84bcf96f3044730a040bcc4626c0b16d
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-08-30 11:35:17 -0300

    add idade_e_inicial.c

---
+++ idade_e_inicial.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int Idade = 95;
+    char Inicial = 'J';
+
+    printf("Idade: %d\n", Idade);
+    printf("Nome: %c\n", Inicial);
+    printf("Cod. ASCII da Inicial: %d!\n", Inicial);
+    printf("Idade em Hex: %02x\n", Idade);
+    return 0;
+}
