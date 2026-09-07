c4dcd35e247717ad064093384b0be7a9202c3c0d
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-09-07 18:28:04 -0300

    Add basic arithmetic operations in aritmeticos.c

---
+++ aula3/aritmeticos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 14;
+    int b = 5;
+
+    printf("soma: %d\n", a+b);
+    printf("subtração: %d\n", a-b);
+    printf("multplicação: %d\n", a*b);
+    printf("divisão: %d\n", a/b);
+    printf("resto: %d\n", a%b);
+
+    return 0;
+}
