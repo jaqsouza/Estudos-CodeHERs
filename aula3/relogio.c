9319202d4ce6adb1310ed4583d131881d8fef3ae
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-09-07 18:32:29 -0300

    Add comments to variables in relogio.c

--- aula3/relogio.c
+++ aula3/relogio.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void){
-    int a = 10;
-    int b = 4;
+    int a = 10; # Hora atual
+    int b = 4;  # Daqui tanto tempo
 
-    printf("%.2x\n", (a+b)%12);
+    printf("%.2x\n", (a+b)%12); # Treinando resto; quanto tempo se passou
 
     return 0;
 }
