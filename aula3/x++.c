50e30390f3fad87f2ff52acbdec33dbdd693a32a
Jaq <131463413+jaqsouza@users.noreply.github.com>
2026-09-09 19:07:06 -0300

    Add x++.c to demonstrate increment operators

---
+++ aula3/x++.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int x = 2;
+    int y = 2;
+
+    printf("%d\n", x++);
+    printf("%d\n", x);
+    printf("%d\n", ++y);
+    printf("%d\n", y);
+
+    return 0;
+}
